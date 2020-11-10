a87a9ea20cdeb7eed913d199d33bd87185e3a5fd
ahernot <70817470+ahernot@users.noreply.github.com>
2020-11-10 18:09:54 +0100

    Update file_analysis.py

--- file_analysis.py
+++ file_analysis.py
@@ -3,8 +3,18 @@ import json
 from subprocess import Popen, PIPE
 
 
+import preferences as Pref
+
+_lsep_ = Pref._lsep_
+
+
 
 def bplist_to_list(bplist: bytes) -> list:
+    """
+    This function decodes bplist bytes strings.
+    :param bplist: The bplist to decode
+    :return: The bplist's list representation
+    """
     # from https://stackoverflow.com/questions/8856032/reading-binary-plist-files-with-python
 
     args = ['plutil', '-convert', 'json', '-o', '-', '--', '-']
@@ -17,8 +27,19 @@ def bplist_to_list(bplist: bytes) -> list:
 
 
 def get_tags(filepath: str) -> list:
+    """
+    This function reads the tags of a file/directory (macOS-specific).
+    :param filepath: The path of the file/directory to analyse
+    :return: The list of tags
+    """
+
     try:
         tagattr = xattr.getxattr(filepath, 'com.apple.metadata:_kMDItemUserTags')
-        return bplist_to_list(tagattr)
+
+        tags_list = bplist_to_list(tagattr)
+
+        tags_list = [tag.split(_lsep_)[0] for tag in tags_list]
+        return tags_list
+
     except:
         return list()
