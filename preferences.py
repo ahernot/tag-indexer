199a475b2a1080cae68219e62b79dda6ad0460a5
ahernot <70817470+ahernot@users.noreply.github.com>
2020-11-10 23:29:44 +0100

    Update preferences.py

--- preferences.py
+++ preferences.py
@@ -1,8 +1,10 @@
 dir_to_process = ''
 
 _files_ = '_files_'
-_dsep_ = '\t'
-_lsep_ = '\n'
-_psep_ = '/'
+
+_dsep_ = '\t' # data separator
+_lsep_ = '\n' # line separator
+_psep_ = '/' # path separator
+_tsep_ = ', ' # tags list separator
 
 database_path = 'database.txt'
