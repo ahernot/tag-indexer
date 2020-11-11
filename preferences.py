762117691d08a414003a4d227248a6f58250dd36
ahernot <70817470+ahernot@users.noreply.github.com>
2020-11-11 12:11:33 +0100

    Update preferences.py

--- preferences.py
+++ preferences.py
@@ -1,12 +1,12 @@
 
 #   IMPORTING THIRD-PARTY LIBRARIES
-# -
+import os
 
 #   IMPORTING PROJECT FILES
 # -
 
 #   DEFINING FILE-WIDE VARIABLES
-# -
+main_path = os.path.realpath(__file__).replace('preferences.py', '')
 
 
 
@@ -24,7 +24,8 @@ _lsep_ = '\n' # line separator
 _psep_ = '/' # path separator
 _tsep_ = ', ' # tags list separator
 
-database_path = 'database.txt'
+
+database_name = 'database.txt' # relative database path
 
 beacon_tags_name = '.pytags_tags'
 beacon_sizes_name = '.pytags_sizes'
