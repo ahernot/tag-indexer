5c9c41faac65c0249823db68413f793e9a60c100
ahernot <70817470+ahernot@users.noreply.github.com>
2020-11-12 16:58:52 +0100

    bug corrections

--- applescript.py
+++ applescript.py
@@ -1,8 +1,13 @@
+"""
+This file handles UNIX alias generation using an AppleScript command run through the Bash terminal.
+Partially adapted from: https://apple.stackexchange.com/questions/278117/how-to-use-applescript-in-a-bash-script-to-create-an-alias-for-an-app
+"""
+
 #   IMPORTING THIRD-PARTY LIBRARIES
 import os
 
 #   IMPORTING PROJECT FILES
-# -
+import basic_functions as BFunc
 
 #   DEFINING FILE-WIDE VARIABLES
 # -
@@ -16,12 +21,25 @@ def make_alias(parent_path: str, child_path: str):
     :return:
     """
 
+    #   1. Extracting the alias target directory
+    child_dir, child_name = BFunc.split_filepath(child_path)
+
+    #   2. Making the alias target directory if it doesn't exist
+    try:
+        os.makedirs(child_dir)
+    except:
+        pass
+
+    #   3. Generating the alias file
+    #   3.1. Generating the command
     applescript_command = f'set filePath to "{parent_path}"\n' \
-                          f'set endPath to "{child_path}"' \
-                          f'\ntell application "Finder"\n' \
+                          f'set endPath to "{child_dir}"\n' \
+                          f'tell application "Finder"\n' \
                           f'make new alias to POSIX file filePath at POSIX file endPath\n' \
+                          f'set name of result to "{child_name}"\n' \
                           f'end tell'
 
     os_command = f'osascript -e \'{applescript_command}\''
 
-    os.system(os_command)
+    #   3.2. Running the command
+    os.popen(os_command)
