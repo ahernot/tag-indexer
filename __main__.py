154179f1714dc88a4a48121f94da505ed672119a
ahernot <70817470+ahernot@users.noreply.github.com>
2020-11-13 09:53:29 +0100

    clean up __main__.py

--- __main__.py
+++ __main__.py
@@ -4,7 +4,6 @@
 # -
 
 #   IMPORTING PROJECT FILES
-import applescript as AS
 import preferences as Pref
 import indexing_functions as IFunc
 
@@ -12,19 +11,8 @@ import indexing_functions as IFunc
 main_directory = Pref.dir_to_process
 
 
-def run():
-
-    #   1. Analysing the main directory if ETA calculation (to know the number of files to process
-    #THIS CODE WILL HAVE TO MOVE TO THE IFunc.process_dir_dict_beacons FUNCTION!!!
-    if Pref.ETA_calculation:
-        dir_dict = IFunc.get_dir_dict(main_directory)
-        ### ADD A BOOL OPTION TO COUNT THE NUMBER OF FILES // GET STATS
-        ### AND A BOOL OPTION TO GET A FLAT LIST OF FILES TOO
-
-    IFunc.process_dir_beacons(main_directory)
-
-
 
 
 if __name__ == '__main__':
-    run()
+
+    IFunc.process_dir_beacons(main_directory)
