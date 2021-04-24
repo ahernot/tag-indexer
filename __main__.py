eaa49b1d0571317ca8e1d5debee93cc5a0ffea9c
ahernot <70817470+ahernot@users.noreply.github.com>
2021-04-24 16:58:54 +0200

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 
-#   IMPORTING THIRD-PARTY LIBRARIES
-# -
-
-#   IMPORTING PROJECT FILES
-import preferences as Pref
 import indexing_functions as IFunc
 
-#   DEFINING FILE-WIDE VARIABLES
-main_directory = Pref.dir_to_process
+
+# Add your dirpath here
+DIRPATH = ''
 
 
+import sys
 
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
 
 if __name__ == '__main__':
 
-    IFunc.process_dir_beacons(main_directory)
+    # IFunc.process_dir_beacons( DIRPATH )
