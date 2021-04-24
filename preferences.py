b2c1de629e3a904b6aa28388df50fda5df08a78f
ahernot <70817470+ahernot@users.noreply.github.com>
2021-04-24 17:23:34 +0200

    Update preferences.py

--- preferences.py
+++ preferences.py
@@ -23,57 +23,9 @@ _tsep_ = ', ' # tags list separator
 _ssep_ = ' ' # space separator
 
 
+alias_dir = 'TEST-generated-aliases/'
 
-
-def read() -> dict:
-    """
-    This function reads the program's preferences file and returns a comprehensive dictionary of the preferences.
-    :return: The dictionary of preferences
-    """
-
-    #   0. Initialising the preferences dictionary
-    preferences_dict = dict()
-
-    #   1. Opening the preferences file
-    try:
-        with open(preferences_path, 'r', encoding='utf-8') as preferences_file:
-            preferences_lines = preferences_file.readlines()
-
-    except:
-        raise FileNotFoundError(f'Unable to found the preferences.txt file in path \'{preferences_path}\'.')
-
-    #   2. Processing the preferences
-    for line in preferences_lines:
-
-        #   2.1. Processing the line to remove line breaks (cannot use basic_functions.py since it calls preferences.py)
-        while '\n' in line:
-            line = line.replace('\n', '')
-
-        #   2.2. Skipping empty lines
-        if not line: continue
-
-        #   2.3. Processing the line
-        line_broken = line.split(_dbsep_)
-
-        #   2.4. Adding the line to the preferences dictionary
-        preferences_dict[line_broken[0]] = _dbsep_.join(line_broken[1:])
-
-
-    return preferences_dict
-
-
-
-
-#   Running the preferences reader
-preferences_dict = read()
-
-
-
-
-#   Generating the needed variables
-dir_to_process = preferences_dict['dir_to_process']
-alias_dir = preferences_dict['alias_dir']
-tag_placeholder = preferences_dict['tag_placeholder']
-database_name = preferences_dict['database_name'] # relative database path
-beacon_tags_name = preferences_dict['beacon_tags_name']
-beacon_sizes_name = preferences_dict['beacon_sizes_name']
+tag_placeholder	= '%'
+database_name = 'database.txt'
+beacon_tags_name = '.pytags_tags'
+beacon_sizes_name = '.pytags_sizes'
