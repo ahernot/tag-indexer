beeb4446fb92b9a331c76208197b62e509ac0383
ahernot <70817470+ahernot@users.noreply.github.com>
2021-04-24 21:42:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python3
 
+"""
+File indexing utility
+@author: Anatole Hernot
+@version: 2.0
+"""
+
 import sys
 
-import indexing_functions as IFunc
+# import indexing_functions as IFunc
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     
-    if len(sys.argv) == 2:  # 3:
-        IFunc.process_dir_beacons( sys.argv[1] )  #, sys.argv[2] )
-        # print( IFunc.get_files_list( sys.argv[1] ) )
+#     if len(sys.argv) == 2:  # 3:
+#         IFunc.process_dir_beacons( sys.argv[1], regenerate_aliases=True )  #, sys.argv[2] )
+#         # print( IFunc.get_files_list( sys.argv[1] ) )
+
+
+## TESTS
+
+import parser
+import indexer
+
+TEST_PATH = '/Users/anatole/Downloads/_TEST_PYTAGS_'
+file_dict = parser.read_dir (TEST_PATH)
+i = indexer.index_files (file_dict)
