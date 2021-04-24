6cbfa57e86a154ba6bbe75cd2e86780ce87a0e23
ahernot <70817470+ahernot@users.noreply.github.com>
2021-04-24 17:27:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,4 @@ if __name__ == '__main__':
     
     if len(sys.argv) == 2:  # 3:
         IFunc.process_dir_beacons( sys.argv[1] )  #, sys.argv[2] )
+        # print( IFunc.get_files_list( sys.argv[1] ) )
