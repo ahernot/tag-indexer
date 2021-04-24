1c9d4a7217cc03cb63b798c71fab0eea2c07e53d
ahernot <70817470+ahernot@users.noreply.github.com>
2021-04-24 21:23:50 +0200

    Create indexer.py
    recursive scanning function

---
+++ indexer.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import os
+
+from preferences import *
+
+
+def read_dir (dirpath: str):
+
+    def recur (dirpath: str, file_dict: dict):
+
+        # Initialise list of subdirs to parse
+        subdirs = list()
+
+        # Generate beacon path (regardless of its existence)
+        beacon_path = os.path.join( dirpath, BEACON_NAME )
+
+        # Initialise dict entry
+        file_dict [beacon_path] = list()
+
+        # Parse current dir
+        with os.scandir( dirpath ) as file_dir:
+
+            for file in file_dir:
+
+                if file.is_dir():
+                    subdirs.append(file.path)
+                
+                else:
+                    file_dict[beacon_path].append( file.path )  # OR append file (posix.DirEntry) instance altogether?
+
+        # Recursively run for subdirs
+        for subdirpath in subdirs:
+            file_dict = recur (subdirpath, file_dict)
+        
+        return file_dict
+    
+    return recur( dirpath, dict() )
