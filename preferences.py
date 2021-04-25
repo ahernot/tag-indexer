07b50ec896a5dc107fa8948d91086579bbc1b821
ahernot <70817470+ahernot@users.noreply.github.com>
2021-04-25 22:38:43 +0200

    add TAGS_DICT

--- preferences.py
+++ preferences.py
@@ -10,6 +10,34 @@ preferences_path = main_path + 'preferences.txt'
 # BEGIN UPDATED FILE
 BEACON_NAME = '.pytags-beacon'
 
+RTAG = '%'
+TAG_NOCAT = 'Others'
+
+# placeholder can only be the last element of the string, and only in subdir
+# for tags with placeholder: [dir, subdir, format-instructions]
+TAGS_DICT = {
+    'Awesome Photo': ['Ratings', 'Awesome Photos'],
+    'Awesome Video': ['Ratings', 'Awesome Videos'],
+    'Great Photo': ['Ratings', 'Great Photos'],
+    'Great Video': ['Ratings', 'Great Videos'],
+    'Great Memories': ['Ratings', 'Great Memories'],
+    'Funny Picture': ['Ratings', 'Funny Picture'],
+
+    f'type {RTAG}': ['Types', RTAG, None],
+    f'camera {RTAG}': ['Cameras', RTAG, None],
+    f'people {RTAG}': ['People', RTAG, None],  # people in the photo/video
+    f'subject {RTAG}': ['Subjects', RTAG, None],  # photo/video subject (not a person)
+    f'shot by {RTAG}': ['Photographers', RTAG, None],
+
+    # Legacy types
+    f'type{RTAG}': ['Types', RTAG, 'space-before-uppercase'],
+    f'camera{RTAG}': ['Cameras', RTAG, 'space-before-uppercase'],
+    f'people{RTAG}': ['People', RTAG, 'space-before-uppercase'],
+    f'subject{RTAG}': ['Subjects', RTAG, 'space-before-uppercase'],
+    f'scene{RTAG}': ['Scenes (legacy)', RTAG, 'space-before-uppercase'],
+    f'shotby{RTAG}': ['Photographers', RTAG, 'space-before-uppercase']
+}
+
 
 
 
@@ -32,3 +60,5 @@ tag_placeholder	= '%'
 database_name = 'database.txt'
 beacon_tags_name = '.pytags_tags'
 beacon_sizes_name = '.pytags_sizes'
+
+
