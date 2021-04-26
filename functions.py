f441bebff7dae32031e996b629baea49b8915d3c
ahernot <70817470+ahernot@users.noreply.github.com>
2021-04-26 20:27:33 +0200

    add docstring, clean up

--- functions.py
+++ functions.py
@@ -1,11 +1,14 @@
+#!/usr/bin/env python3
 
 def dictlen (tag_dict: dict):
+    """
+    Get the total length of all the elements of a dict like {key: [elements_list]}
+    :param tag_dict: dictionary to analyse
+    :return: combined length of the dictionary's elements
+    """
     l = 0
 
     for tag in tag_dict:
         l += len (tag_dict [tag])
     
     return l
-
-
-# folder cleaner function to run at the end to find empty folders and delete them
