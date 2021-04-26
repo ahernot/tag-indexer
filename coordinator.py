94583e40c154a50d6bf3ce09e614637625bdeae0
ahernot <70817470+ahernot@users.noreply.github.com>
2021-04-26 13:22:00 +0200

    add print

--- coordinator.py
+++ coordinator.py
@@ -4,6 +4,8 @@ from parser import read_dir
 from indexer import index_files
 from tag_processer import add_aliases, remove_aliases
 
+from functions import dictlen
+
 
 def process_dir (dirpath: str, output_dirpath: str, ignore_beacons = False):
 
@@ -12,6 +14,8 @@ def process_dir (dirpath: str, output_dirpath: str, ignore_beacons = False):
 
     # Index files according to tags
     tag_add_dict, tag_remove_dict = index_files (file_dict, ignore_beacons=ignore_beacons)
+
+    print (f'Adding {dictlen(tag_add_dict)} aliases.\nRemoving {dictlen(tag_remove_dict)} aliases.')
 
     # Make aliases
     add_aliases (output_dirpath, tag_add_dict)
