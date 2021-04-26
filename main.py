1f9a491d1e1eae382450ca3f5e791e1264a016db
ahernot <70817470+ahernot@users.noreply.github.com>
2021-04-26 13:10:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     
     if len(sys.argv) == 3:
 
-        process_dir (sys.argv[1], sys.argv[2], ignore_beacons=True)
+        process_dir (sys.argv[1], sys.argv[2], ignore_beacons=False)
 
 
 # COMMAND: python3.9 main.py /Users/anatole/Downloads/_TEST_PYTAGS_ /Users/anatole/Documents/GitHub/tag-indexer/_tests/TEST-generated-aliases
