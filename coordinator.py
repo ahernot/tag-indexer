8d26797b8f9bb7669faf704927e145e88c847a3c
ahernot <70817470+ahernot@users.noreply.github.com>
2021-04-26 13:10:44 +0200

    add add_aliases() call

--- coordinator.py
+++ coordinator.py
@@ -2,6 +2,7 @@
 
 from parser import read_dir
 from indexer import index_files
+from tag_processer import add_aliases, remove_aliases
 
 
 def process_dir (dirpath: str, output_dirpath: str, ignore_beacons = False):
@@ -12,9 +13,8 @@ def process_dir (dirpath: str, output_dirpath: str, ignore_beacons = False):
     # Index files according to tags
     tag_add_dict, tag_remove_dict = index_files (file_dict, ignore_beacons=ignore_beacons)
 
-    print(tag_add_dict, tag_remove_dict)
-
     # Make aliases
+    add_aliases (output_dirpath, tag_add_dict)
 
     # Destroy aliases
 
