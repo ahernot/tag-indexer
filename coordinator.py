1b724aec1d71e86a4c19aa88c3d283caef098a56
ahernot <70817470+ahernot@users.noreply.github.com>
2021-04-27 13:02:46 +0200

    begin log implementation

--- coordinator.py
+++ coordinator.py
@@ -4,12 +4,15 @@ from parser import read_dir
 from indexer import index_files
 from tag_processer import add_aliases, remove_aliases
 
+from logger import Log
+
 from functions import dictlen
 
 
 def process_dir (dirpath: str, output_dirpath: str, ignore_beacons = False):
 
-    LOG = str()
+    # Instantiate log
+    log = Log (dirpath=output_dirpath)
 
     # Parse directory
     file_dict = read_dir (dirpath)
@@ -18,6 +21,7 @@ def process_dir (dirpath: str, output_dirpath: str, ignore_beacons = False):
     tag_add_dict, tag_remove_dict = index_files (file_dict, ignore_beacons=ignore_beacons)
 
     print (f'Adding {dictlen(tag_add_dict)} aliases.\nRemoving {dictlen(tag_remove_dict)} aliases.')
+    # add to log
 
     # Add aliases
     add_aliases (output_dirpath, tag_add_dict)
@@ -25,4 +29,5 @@ def process_dir (dirpath: str, output_dirpath: str, ignore_beacons = False):
     # Remove aliases
     remove_aliases (output_dirpath, tag_remove_dict)
 
-    # Update log
+    # Save log
+    log.save()
