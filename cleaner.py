c6565f8b1269ddca7e02f5db60ef91eb8f1c43aa
ahernot <70817470+ahernot@users.noreply.github.com>
2021-04-27 13:48:56 +0200

    create remove_beacons()

--- cleaner.py
+++ cleaner.py
@@ -1,3 +1,49 @@
-# remove .pytags-dirtype
-# remove .pytags-beacon
-# folder cleaner function to run at the end to find empty folders and delete them
+#!/usr/bin/env python3
+
+import os
+
+from preferences import *
+
+
+def remove_beacons (dirpath: str, beacon_name: str = BEACON_NAME):
+    """
+    Recursively remove all beacons from a directory and its subdirectories
+    :param dirpath: path of the directory to alter
+    :param beacon_name: filename of the beacon
+    :return: None
+    """
+    
+    def recur (dirpath: str, beacon_pathlist: list):
+
+        # Initialise list of subdirs to parse
+        subdirs = list()
+
+        # Parse current dir
+        with os.scandir (dirpath) as file_dir:
+
+            for file in file_dir:
+
+                if file.is_dir():
+                    subdirs.append (file.path)
+                
+                else:
+                    if file.name == beacon_name:
+                        os.remove (file.path)
+
+
+        # Recursively run for subdirs
+        for subdirpath in subdirs:
+            file_dict = recur (subdirpath, beacon_pathlist)
+        
+        return beacon_pathlist
+
+    beacon_pathlist = recur (dirpath, list())
+
+
+def clean_dir (dirpath: str):
+    """
+    Remove empty children folders within dirpath
+    :param dirpath: path of the directory to alter
+    :return: None
+    """
+    pass
