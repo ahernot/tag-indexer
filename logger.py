3a5f8c4c68b31a029a410bb1db42eb8ae061167a
ahernot <70817470+ahernot@users.noreply.github.com>
2021-04-27 13:02:39 +0200

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import os
+import datetime
+
+from preferences import *
+
+
+def get_datetime ():
+    return datetime.datetime.now () .strftime (LOG_DATETIME_FORMAT)
+
+
+class Log:
+
+    def __init__ (self, dirpath: str):
+        self._content_add = list()
+
+        self.path = os.path.join (dirpath, LOG_FILENAME)
+
+        if not os.path.exists (self.path):
+            self._reset()
+        
+    def _reset (self):
+        with open (self.path, 'w', encoding='utf-8') as log:
+            pass
+
+    def add_line (self, line: str):
+        self._content_add += line
+
+    def save (self):
+        dt = get_datetime ()
+
+        with open (self.path, 'a', encoding='utf-8') as log:
+            for line in self._content_add:
+                log.write (f'{dt} – {line}\n')
